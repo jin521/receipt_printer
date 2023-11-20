16faba211bdc5fcb5cf4cf5a03cdfe92fe540e40
jean <autumn.qiujin@gmail.com>
2023-11-21 00:17:13 +1100

    fix test

--- spec/services/receipt_builder_spec.rb
+++ spec/services/receipt_builder_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe ReceiptBuilder do
   describe '.build' do
     it 'builds a receipt from CSV file' do
       allow(CSV).to receive(:foreach).with(file_path, headers: true, col_sep: ',').and_yield(
-        CSV::Row.new(%w[Quantity Product Price], ['2', 'music cd', '14.99'])
+        CSV::Row.new(%w[Quantity Product Price], ['1', 'book', '14.99'])
       ).and_yield(
-        CSV::Row.new(%w[Quantity Product Price], ['1', 'chocolate bar', '0.85'])
+        CSV::Row.new(%w[Quantity Product Price], ['1', 'music cd', '0.85'])
       )
 
       receipt_builder = ReceiptBuilder.build(file_path)
@@ -23,18 +23,13 @@ RSpec.describe ReceiptBuilder do
       expect(product1).to be_a(Product)
       expect(product1.quantity).to eq(1)
       expect(product1.name).to eq('book')
-      expect(product1.price).to eq(12.49)
+      expect(product1.price).to eq(14.99)
 
       product2 = receipt_builder.products[1]
       expect(product2).to be_a(Product)
       expect(product2.quantity).to eq(1)
       expect(product2.name).to eq('music cd')
-      expect(product2.price).to eq(14.99)
-
-      expect(receipt_builder.sales_tax).to be_nil
-      expect(receipt_builder.total).to be_nil
-
-      receipt_builder.calculate_totals
+      expect(product2.price).to eq(0.85)
 
       expect(receipt_builder.sales_tax).to_not be_nil
       expect(receipt_builder.total).to_not be_nil
